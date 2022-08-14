c62d952ce038b6f3106bb6fcfbb3afb1cf978e55
Pride <roachl9215@gmail.com>
2022-08-15 11:53:37 +1200

    third commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ import {
 } from "react-router-dom";
 
 import Home from "./pages/Home";
-import SEPractice from "./pages/SE-Practice";
+import SEPractice from "./pages/SE-Practices";
 import SubmitArticle from "./pages/Submit-Article"; 
 import NotFoundPage from "./pages/404";
 const App = () =>  {
@@ -17,12 +17,12 @@ const App = () =>  {
           <h1>Software Practice Empirical Evidence Database (SPEED)</h1>
             <ul className="header">
               <li><NavLink exact to = "/">Home</NavLink></li>
-              <li><NavLink to = "/SEPractice">Select the Practice</NavLink></li>
+              <li><NavLink to = "/SEPractices">Select the Practice</NavLink></li>
               <li><NavLink to = "/SubmitArticle">Submit an Article</NavLink></li>
             </ul>
           <div className="content">
             <Route exact path="/" component={Home}/>
-            <Route  path="/SEPractice" component={SEPractice}/>
+            <Route  path="/SEPractices" component={SEPractice}/>
             <Route  path="/SubmitArticle" component={SubmitArticle}/>
             <Route exact path="/404" component={NotFoundPage}/>
             <Redirect to="/404" />
